c8c2c5572ec0a493b405ed9c5e5bf5c998637e94
Jonathan Stewart <jonjonstew09@gmail.com>
2026-02-04 09:19:07 -0500

    Add updated budget calculator with new logic features
    This file contains an updated version of the budget calculator with new logic features.

---
+++ budget_calculator2.py
@@ -0,0 +1,10 @@
+"""
+This is an updated version of the previous budget calculator which 
+implements the use of if/then/else, switch statements, and boolean variables
+"""
+
+# Declare variables
+
+# Logic and testing
+
+# Print the output
