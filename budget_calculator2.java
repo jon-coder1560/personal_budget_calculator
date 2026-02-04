bfaad8f66ed0dbcbc6cee0b5e43f729790170ef5
Jonathan Stewart <jonjonstew09@gmail.com>
2026-02-04 09:16:19 -0500

    Create BudgetCalculator2 class with initial setup
    Initial implementation of BudgetCalculator2 with comments and structure.

---
+++ budget_calculator2.java
@@ -0,0 +1,19 @@
+/* 
+* This is an updated version of the previous budget calculator which 
+* implements the use of if/then/else, switch, and boolean variables
+* Will be finished by end of week
+* 
+* @author Jonathan
+*/
+
+import java.util.Scanner;
+
+public class BudgetCalculator2 {
+  public static void main(String[] args) {
+    // Declare variables
+
+    // Logic and testing
+
+    // Print the output
+  }
+}
