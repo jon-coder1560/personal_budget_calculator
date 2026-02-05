3da1f5dfdc9076337b4ceafa0e937e21a174b3db
Jonathan Stewart <jonjonstew09@gmail.com>
2026-02-05 11:46:09 -0500

    Update completion date in budget calculator comments

--- budget_calculator2.java
+++ budget_calculator2.java
@@ -1,7 +1,7 @@
 /* 
 * This is an updated version of the previous budget calculator which 
 * implements the use of if/then/else, switch, and boolean variables
-* Will be finished by end of week
+* Will be finished between 2/9 and 2/11
 * 
 * @author Jonathan
 */
